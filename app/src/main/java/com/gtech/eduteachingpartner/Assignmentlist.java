f1cb4cc8317e1208c079a3ed349133f145c73cf4
Govind yadav <43997191+GtechGovind@users.noreply.github.com>
2021-02-28 19:09:16 +0530

    cradview for assignment done

--- app/src/main/java/com/gtech/eduteachingpartner/Assignmentlist.java
+++ app/src/main/java/com/gtech/eduteachingpartner/Assignmentlist.java
@@ -3,19 +3,12 @@ package com.gtech.eduteachingpartner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class Assignment extends AppCompatActivity {
+public class Assignmentlist extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_assignment);
-    }
-
-    public void thirdyear(View view) {
-    }
-
-    public void secondyear(View view) {
+        setContentView(R.layout.activity_assignmentlist);
     }
 }
